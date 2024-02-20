3ee2a292aa1e09e8f2dcdc3ae3507578acb9c3db
j.waschinski <j.waschinski@orgacard.de>
2024-02-20 15:53:15 +0100

    renamed settings.json to data.json
    started implementing loadingSettings

--- scripts/frontendScipt.js
+++ scripts/frontendScipt.js
@@ -1,16 +1,18 @@
 const { ipcRenderer } = window; // Use provided ipcRenderer in Electron apps
+const settingsFile = ('../SMMDownloader/data.json');
+let SettingsData;
 
 
 function CEMUcheckBoxChanged() {
     if (document.getElementById("useCEMUfolderValue").value == "false") {
         document.getElementById("useCEMUfolderValue").value = "true"
-        document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder (Coming Soon)</button><br><br>`
+        document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder</button><br><br>`
     } else if (document.getElementById("useCEMUfolderValue").value == "true") {
         document.getElementById("useCEMUfolderValue").value = "false"
         document.getElementById('optionalCEMU').innerHTML = ``
     } else {
         document.getElementById("useCEMUfolderValue").value = "true"
-        document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder (Coming Soon)</button><br><br>`
+        document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder</button><br><br>`
     }
 }
 
@@ -55,8 +57,18 @@ function loadPage(page) {
         .then(response => response.text())
         .then(data => {
             document.getElementById('main-content').innerHTML = data;
+            loadPageScripts(page);
         })
         .catch(error => console.log(error));
+}
+
+function loadPageScripts(page) {
+    if (page == "../pages/settings.html") {
+        if (SettingsData.useCemuDir == true) {
+            document.getElementById("useCEMUfolder").outerHTML += ("checked")
+            document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder</button><br><br>`
+        }
+    }
 }
 
 function saveSettings() {
@@ -64,7 +76,29 @@ function saveSettings() {
 }
 
 function loadSettings() {
+    fetch(settingsFile)
+        .then(response => response.json())
+        .then(data => {
+            SettingsData = data;
+            //onsole.log(data);
+            /*
+            // Selected Folder
+            if (data.CemuDirPath != "") {
+                document.getElementById('selectedFolderValue').value = data.CemuDirPath;
+                console.log(data.CemuDirPath);
+                console.log(document.getElementById('selectedFolderValue').value)
+                //document.getElementById('selectedFolder').innerHTML = `<h2>Selected Folder: ${data}</h2>`
+            }
 
+            if (data.useCemuDir != "false") {
+                console.log(data.useCemuDir);
+                console.log(document.getElementById('useCEMUfolderValue').value)
+                document.getElementById('useCEMUfolderValue').value = data.useCemuDir;
+                //document.getElementById('optionalCEMU').innerHTML = `<h2>Select Cemu Folder:</h2><button onclick="selectFolder()">Select Folder</button><br><br>`
+            }*/
+
+        })
+        .catch(error => console.log(error));
 }
 
 window.addEventListener('DOMContentLoaded', () => {
